89684da18fd6f93bb51dc37c31818c01d29b0ea3
omprakash upadhaya <Omprakash.Upadhaya@emc.com>
2018-10-14 13:46:38 +0530

    "Added task 2 method"

--- SeleniumTesting/src/test/java/Testing/Test2.java
+++ SeleniumTesting/src/test/java/Testing/Test2.java
@@ -5,6 +5,9 @@ public class Test2 {
 		public static void task() {
 			System.out.println("Hello Test2");
 		}
+		public static void task2() {
+			System.out.println("task 2 method created");
+		}
 	public static void main(String[] args) {
 		
 
