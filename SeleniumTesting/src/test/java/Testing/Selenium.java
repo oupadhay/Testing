7c46d6f0ad7e5ec807f848f85c30222ce3d8c9c0
omprakash upadhaya <Omprakash.Upadhaya@emc.com>
2018-10-14 13:12:56 +0530

    "Div,Mul by om"

--- SeleniumTesting/src/test/java/Testing/Selenium.java
+++ SeleniumTesting/src/test/java/Testing/Selenium.java
@@ -11,6 +11,12 @@ public class Selenium {
 	public static void rain() {
 		System.out.println("Addition");
 	}
+	public static void div() {
+		System.out.println("Addition");
+	}
+	public static void mul() {
+		System.out.println("Addition");
+	}
 	public static void main(String[] args) {
 		add();
 	}
