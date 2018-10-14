2c67d03c35b2e54dd65a86b6966aa8bd5d9e98fa
omprakash upadhaya <Omprakash.Upadhaya@emc.com>
2018-10-14 12:43:32 +0530

    "By OM"

--- SeleniumTesting/src/test/java/Testing/Selenium.java
+++ SeleniumTesting/src/test/java/Testing/Selenium.java
@@ -5,6 +5,12 @@ public class Selenium {
 	public static void add() {
 		System.out.println("Addition");
 	}
+	public static void sub() {
+		System.out.println("Addition");
+	}
+	public static void rain() {
+		System.out.println("Addition");
+	}
 	public static void main(String[] args) {
 		add();
 	}
