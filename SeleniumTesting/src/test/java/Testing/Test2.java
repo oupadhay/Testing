cd69303c3f43b011d5f207e39d2f39bd0090c932
malli <malli.korm@gmail.com>
2018-10-14 13:45:35 +0530

    "added task 1 method"

--- SeleniumTesting/src/test/java/Testing/Test2.java
+++ SeleniumTesting/src/test/java/Testing/Test2.java
@@ -5,6 +5,9 @@ public class Test2 {
 		public static void task() {
 			System.out.println("Hello Test2");
 		}
+		public static void task1() {			
+		System.out.println("Task1");
+		}
 	public static void main(String[] args) {
 		
 
