ffd9cd1c3c7a030fbca0ab4fd3c139d08b514fda
malli <malli.korm@gmail.com>
2018-10-14 13:50:38 +0530

    "task3 addded"

--- SeleniumTesting/src/test/java/Testing/Test2.java
+++ SeleniumTesting/src/test/java/Testing/Test2.java
@@ -11,6 +11,9 @@ public class Test2 {
 		public static void task1() {			
 			System.out.println("Task1");
 		}
+		public static void task4() {			
+			System.out.println("Task1");
+		}
 	public static void main(String[] args) {
 		
 
